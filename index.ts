916ad56930844f7065e369931b4ccfc5e931b530
Amrit Rai <iamamrit27@gmail.com>
2024-09-10 18:20:31 +0530

    chore: Update redis-parser and redis-errors dependencies

--- index.ts
+++ index.ts
@@ -1,10 +1,47 @@
 const net = require('net');
+const Parser = require('redis-parser');
+const store: never[] = [];
 
-const server = net.createServer(((connection: { on: (arg0: string, arg1: (data: any) => void) => void; }) => {
+const server = net.createServer(((connection: {
+    [x: string]: any; on: (arg0: string, arg1: (data: any) => void) => void; 
+}) => {
     console.log('Client connected...');
 
     connection.on('data', data => {
-        console.log(data.toString());
+        const parser = new Parser({
+            returnReply: (reply: any) => {
+                const command = reply[0];
+                switch(command){
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        if (typeof key === 'string') {
+                            (store as Record<string, any>)[key] = value;
+                        } else {
+                            connection.write('-ERR Invalid key type\r\n');
+                            break;
+                        }
+                        connection.write('+OK\r\n');
+                    }
+                    break;
+                    case 'get': {
+                        const key = reply[1];
+                        const value = store[key];
+                        connection.write(`+${value}\r\n`);
+                    }
+                    break;
+                    default: {
+                        
+                    }
+                }
+            },
+            returnError: (error: any) => {
+                console.log(error);
+            }
+        })
+
+        parser.execute(data);
+        connection.write('+0K\r\n');
     });
 }));
 
